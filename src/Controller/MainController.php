2e31f8814bc9d745ec43866bad181d2cc3c7f9fa
JoshuaSao <jsao@gmx.com>
2020-09-25 17:16:55 +0200

    trun commmun 004 bug reglé dans le main controller

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -12,7 +12,7 @@ class MainController extends AbstractController
      */
     public function index()
     {
-        return $this->render('main/layout.html.twig', [
+        return $this->render('layout.html.twig', [
             'controller_name' => 'MainController',
         ]);
     }
