93219098b3cf1cdd297bbdfb81cc583f9c59426e
Audrey Tronel <audreytronel@outlook.fr>
2020-09-25 14:46:20 +0200

    Ajout lien one to many

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -4,10 +4,10 @@ namespace App\Form;
 
 use App\Entity\Campus;
 use App\Entity\User;
-use Doctrine\DBAL\Types\IntegerType;
+
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -16,7 +16,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormTypeInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\Choice;
+
 
 class RegisterType extends AbstractType implements FormTypeInterface
 {
